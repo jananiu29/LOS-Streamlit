f11c526f370abb799310410c6fcdfda0ff29b1e0
jananiu29 <157469891+jananiu29@users.noreply.github.com>
2024-02-14 00:02:14 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import StandardScaler
 import streamlit as st
